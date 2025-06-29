d2abeed53ca65268178c6bf640d54e6bcc6f1e0a
descawed <github@descawed.com>
2025-06-28 21:05:25 -0400

    Enhance animation parsing to account for the fact that animations can also come from RDTs

--- src/re2/animation.rs
+++ src/re2/animation.rs
@@ -68,27 +68,30 @@ impl AnimationFrame {
 }
 
 #[derive(Debug, Clone)]
-pub struct WeaponModel {
+pub struct AnimationSet {
     animations: Vec<Vec<AnimationFrame>>,
-    mesh_data: Vec<u8>,
-    tim_data: Vec<u8>,
+    character_mask: u32,
 }
 
-impl WeaponModel {
-    pub fn read<T: Read + Seek>(mut f: T) -> Result<Self> {
-        let directory_offset: u32 = f.read_le()?;
-        let num_directory_entries: u32 = f.read_le()?;
+impl AnimationSet {
+    pub const fn new(animations: Vec<Vec<AnimationFrame>>, character_mask: u32) -> Self {
+        Self { animations, character_mask }
+    }
 
-        if num_directory_entries != 4 {
-            bail!("Unexpected number of sections in PLW file");
-        }
+    pub const fn from_model(animations: Vec<Vec<AnimationFrame>>) -> Self {
+        Self::new(animations, 0xffffffff)
+    }
 
-        f.seek(SeekFrom::Start(directory_offset as u64))?;
+    pub const fn animations(&self) -> &[Vec<AnimationFrame>] {
+        self.animations.as_slice()
+    }
 
-        let directory: [u32; 4] = f.read_le()?;
+    pub const fn character_mask(&self) -> u32 {
+        self.character_mask
+    }
 
-        let animation_steps_section_offset = directory[0] as u64;
-        f.seek(SeekFrom::Start(animation_steps_section_offset))?;
+    fn read_steps<T: Read + Seek>(mut f: T, start: u64) -> Result<(Vec<Vec<FrameFlags>>, usize)> {
+        f.seek(SeekFrom::Start(start))?;
 
         let first_entry: AnimationHeaderEntry = f.read_le()?;
         let num_animations = first_entry.data_offset as usize / size_of::<AnimationHeaderEntry>();
@@ -103,49 +106,33 @@ impl WeaponModel {
         let mut animation_flags = Vec::with_capacity(num_animations);
         let mut total_frames = 0usize;
         for entry in &animation_directory {
-            f.seek(SeekFrom::Start(animation_steps_section_offset + entry.data_offset as u64))?;
+            f.seek(SeekFrom::Start(start + entry.data_offset as u64))?;
             let frame_flags: Vec<FrameFlags> = f.read_le_args(VecArgs { count: entry.num_frames as usize, inner: () })?;
-            total_frames += frame_flags.len();
+            total_frames = total_frames.max(frame_flags.iter().map(FrameFlags::index).max().unwrap_or(0) + 1);
             animation_flags.push(frame_flags);
         }
 
-        let animation_frames_section_offset = directory[1] as u64;
-        f.seek(SeekFrom::Start(animation_frames_section_offset))?;
+        Ok((animation_flags, total_frames))
+    }
+
+    fn read_frames<T: Read + Seek>(mut f: T, start: u64, total_frames: usize) -> Result<Vec<FrameMotionData>> {
+        f.seek(SeekFrom::Start(start))?;
 
         let header: FramesHeader = f.read_le()?;
         let element_size = header.element_size as usize;
 
-        let frame_motion = if header.motion_offset == 0 || element_size < MIN_MOTION_SIZE {
+        Ok(if header.motion_offset == 0 || element_size < MIN_MOTION_SIZE {
             Vec::new()
         } else {
-            f.seek(SeekFrom::Start(animation_frames_section_offset + header.motion_offset as u64))?;
+            f.seek(SeekFrom::Start(start + header.motion_offset as u64))?;
 
             let frame_motion: Vec<FrameMotionData> = f.read_le_args(VecArgs { count: total_frames, inner: FrameMotionDataBinReadArgs { byte_size: element_size } })?;
             frame_motion
-        };
+        })
+    }
 
-        let mesh_data_size = directory[3].saturating_sub(directory[2]) as usize;
-        let tim_data_size = directory_offset.saturating_sub(directory[3]) as usize;
-
-        let mesh_data = if mesh_data_size > 0 {
-            f.seek(SeekFrom::Start(directory[2] as u64))?;
-            let mut mesh_data = vec![0u8; mesh_data_size];
-            f.read_exact(&mut mesh_data)?;
-            mesh_data
-        } else {
-            Vec::new()
-        };
-
-        let tim_data = if tim_data_size > 0 {
-            f.seek(SeekFrom::Start(directory[3] as u64))?;
-            let mut tim_data = vec![0u8; tim_data_size];
-            f.read_exact(&mut tim_data)?;
-            tim_data
-        } else {
-            Vec::new()
-        };
-
-        let mut animations = Vec::with_capacity(num_animations);
+    fn combine_data(animation_flags: Vec<Vec<FrameFlags>>, frame_motion: Vec<FrameMotionData>) -> Vec<Vec<AnimationFrame>> {
+        let mut animations = Vec::with_capacity(animation_flags.len());
         if !frame_motion.is_empty() {
             for frame_flags in animation_flags {
                 let mut frames = Vec::with_capacity(frame_flags.len());
@@ -160,23 +147,54 @@ impl WeaponModel {
             }
         }
 
-        Ok(Self {
-            animations,
-            mesh_data,
-            tim_data,
-        })
+        animations
     }
 
-    pub const fn animations(&self) -> &[Vec<AnimationFrame>] {
-        self.animations.as_slice()
+    pub fn read_plw<T: Read + Seek>(mut f: T) -> Result<Self> {
+        let directory_offset: u32 = f.read_le()?;
+        let num_directory_entries: u32 = f.read_le()?;
+
+        if num_directory_entries < 2 {
+            bail!("Not enough sections to read animation data");
+        }
+
+        f.seek(SeekFrom::Start(directory_offset as u64))?;
+
+        let directory: Vec<u32> = f.read_le_args(VecArgs { count: num_directory_entries as usize, inner: () })?;
+
+        let animation_steps_section_offset = directory[0] as u64;
+        let (animation_flags, total_frames) = Self::read_steps(&mut f, animation_steps_section_offset)?;
+
+        let animation_frames_section_offset = directory[1] as u64;
+        let frame_motion = Self::read_frames(&mut f, animation_frames_section_offset, total_frames)?;
+
+        Ok(Self::from_model(Self::combine_data(animation_flags, frame_motion)))
     }
 
-    pub const fn mesh_data(&self) -> &[u8] {
-        self.mesh_data.as_slice()
-    }
+    pub fn read_rdt<T: Read + Seek>(mut f: T) -> Result<Vec<Self>> {
+        let start = f.stream_position()?;
 
-    pub const fn tim_data(&self) -> &[u8] {
-        self.tim_data.as_slice()
+        let directory_offset: u32 = f.read_le()?;
+        let num_directory_entries: u32 = f.read_le()?;
+
+        f.seek(SeekFrom::Start(start + directory_offset as u64))?;
+
+        let directory: Vec<(u32, u32)> = f.read_le_args(VecArgs { count: num_directory_entries as usize, inner: () })?;
+
+        let mut animation_sets = Vec::with_capacity(num_directory_entries as usize);
+        for (frames_offset, steps_offset) in directory {
+            let animation_steps_section_offset = steps_offset as u64;
+            let (animation_flags, total_frames) = Self::read_steps(&mut f, start + animation_steps_section_offset)?;
+
+            let animation_frames_section_offset = frames_offset as u64;
+            f.seek(SeekFrom::Start(start + animation_frames_section_offset))?;
+            let character_mask: u32 = f.read_le()?;
+            let frame_motion = Self::read_frames(&mut f, start + animation_frames_section_offset + 4, total_frames)?;
+
+            animation_sets.push(Self::new(Self::combine_data(animation_flags, frame_motion), character_mask));
+        }
+
+        Ok(animation_sets)
     }
 }
 
@@ -189,14 +207,27 @@ mod tests {
     #[test]
     fn read_plw() {
         let file = File::open(r"D:\games\BIOHAZARD 2 PC\pl0\PLD\PL0EW11.PLW").unwrap();
-        let weapon_model = WeaponModel::read(file).unwrap();
-        assert!(weapon_model.animations.len() > 0);
+        let set = AnimationSet::read_plw(file).unwrap();
+        assert!(set.animations.len() > 0);
     }
 
     #[test]
     fn read_plw_with_no_animations() {
         let file = File::open(r"D:\games\BIOHAZARD 2 PC\pl0\PLD\PL0EW10.PLW").unwrap();
-        let weapon_model = WeaponModel::read(file).unwrap();
-        assert_eq!(weapon_model.animations.len(), 0);
+        let set = AnimationSet::read_plw(file).unwrap();
+        assert_eq!(set.animations.len(), 0);
+    }
+
+    #[test]
+    fn read_rdt() {
+        let mut file = File::open(r"D:\games\BIOHAZARD 2 PC\pl0\Rdt\ROOM10C0.RDT").unwrap();
+        file.seek(SeekFrom::Start(0x60)).unwrap();
+        let animation_offset: u32 = file.read_le().unwrap();
+        file.seek(SeekFrom::Start(animation_offset as u64)).unwrap();
+
+        let sets = AnimationSet::read_rdt(file).unwrap();
+        assert!(sets.len() > 0);
+        assert_ne!(sets[0].character_mask & 1, 0);
+        assert!(sets[0].animations.len() > 0);
     }
 }
